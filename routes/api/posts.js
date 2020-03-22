bebe7d08e9d8481a04937a699306d65b9c8b804d
elitecolors <saidaniahmed125@gmail.com>
2020-03-22 12:26:05 +0100

    get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -45,4 +45,83 @@ router.post ('/',[auth,[
     
 });
 
+// get All  Post 
+
+router.get('/',auth,async (req,res)=>{
+    try {
+        const posts =  await Post.find().sort({
+            date : -1
+        });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// get post by id 
+router.get('/:id',auth,async (req,res)=>{
+    try {
+            
+        const post =  await Post.findById(req.params.id).sort({
+            date : -1
+        });
+        if(!post){
+            return res.status(404).json({
+                msg : 'Post not found'
+            });
+        }
+
+        
+        res.json(post);
+    } catch (error) {
+        if(error.kind==='ObjectId'){
+            return res.status(404).json({
+                msg : 'Post not found'
+            });
+        }
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// delete post by id 
+router.delete('/:id',auth,async (req,res)=>{
+    try {
+            
+        const post =  await Post.findById(req.params.id).sort({
+            date : -1
+        });
+
+        if(!post){
+            return res.status(404).json({
+                msg : 'Post not found'
+            });
+        }
+        // check user 
+        if(post.user.toString() !== req.user.id){
+            return res.status(401).json({
+                msg : 'User not auth'
+            });
+        }
+
+        await post.remove();
+        
+        res.json({
+            msg: 'Post deleted'
+        });
+        
+    } catch (error) {
+        if(error.kind==='ObjectId'){
+            return res.status(404).json({
+                msg : 'Post not found'
+            });
+        }
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
 module.exports = router;
